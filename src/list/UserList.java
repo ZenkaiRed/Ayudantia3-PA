d6e848231c333e0b6b3e8cf916358383adf9a65d
ZenkaiRed <marcelo.cespedes@alumnos.ucn.cl>
2024-04-05 13:17:01 -0300

    feat: Clase UserList completada

--- src/list/UserList.java
+++ src/list/UserList.java
@@ -4,10 +4,15 @@ import model.User;
 
 public class UserList {
 
-    private User[] users;
-    private int maxQuantity;
+    // Una variable final significa que nunca sufrirá cambios.
+    private final User[] users;
+    private final int maxQuantity;
     private int quantity;
 
+    /**
+     * Constructor de la clase UserList.
+     * @param maxQuantity Corresponde a la capacidad máxima que tendrá la lista.
+     */
     public UserList(int maxQuantity) {
 
         if (maxQuantity < 0){
@@ -19,6 +24,11 @@ public class UserList {
         this.quantity = 0;
     }
 
+    /**
+     * Método que permite agregar un usuario a la lista.
+     * @param user Usuario a agregar.
+     * @return Retorna un booleano que indica si se pudo agregar el usuario o no.
+     */
     public boolean addUser(User user) {
 
         if (this.quantity >= this.maxQuantity) {
@@ -41,11 +51,24 @@ public class UserList {
         return true;
     }
 
+    /**
+     * Método que permite agregar un usuario a la lista.
+     * @param firstName Nombre del usuario.
+     * @param lastName Apellido del usuario.
+     * @param rut Rut del usuario.
+     * @param membership Membresía del usuario.
+     * @return Retorna un booleano que indica si se pudo agregar el usuario o no.
+     */
     public boolean addUser(String firstName, String lastName, String rut, String membership){
         User user = new User(firstName, lastName, rut, membership);
         return this.addUser(user);
     }
 
+    /**
+     * Método que permite buscar un usuario en la lista.
+     * @param rut Rut del usuario a buscar.
+     * @return Retorna la posición del usuario en la lista.
+     */
     public int searchUser(String rut){
 
         for (int i = 0; i < this.quantity; i++) {
@@ -57,6 +80,11 @@ public class UserList {
         return -1;
     }
 
+    /**
+     * Método que permite obtener los datos de un usuario en la lista.
+     * @param rut Rut del usuario.
+     * @return Retorna los datos del usuario.
+     */
     public String getUser(String rut){
 
         int position = this.searchUser(rut);
@@ -67,27 +95,112 @@ public class UserList {
 
         User user = this.users[position];
 
-        String data = "Nombre: " + user.getFirstName() +
-                "\nApellido: " + user.getLastName() +
-                "\nRUT: " + user.getRut() +
-                "\nMembresía: " + user.getMembership();
+        return user.toString();
+    }
 
-        return data;
+    /**
+     * Método que permite obtener los datos de un usuario en la lista.
+     * @param position Posición del usuario en la lista.
+     * @return Retorna los datos del usuario.
+     */
+    public String getUser(int position){
+        if (position < 0 || position >= this.quantity){
+            return null;
+        }
+
+        User user = this.users[position];
+        return user.toString();
+    }
+
+    /**
+     * Método que permite eliminar un usuario de la lista.
+     * @param rut Rut del usuario.
+     * @return Retorna un booleano que indica si se pudo eliminar el usuario o no.
+     */
+    public boolean deleteUser(String rut){
+        int position = this.searchUser(rut);
+        if (position == -1){
+            return false;
+        }
+
+        for (int i = position; i < this.quantity - 1; i++) {
+            this.users[i] = this.users[i + 1];
+        }
+
+        this.quantity--;
+        return true;
+    }
+
+    /**
+     * Método que permite eliminar un usuario de la lista.
+     * @param position Posición del usuario en la lista.
+     * @return Retorna un booleano que indica si se pudo eliminar el usuario o no.
+     */
+    public boolean deleteUser(int position) {
+        if (position < 0 || position >= this.quantity) {
+            return false;
+        }
+
+        for (int i = position; i < this.quantity - 1; i++) {
+            this.users[i] = this.users[i + 1];
+        }
+
+        this.quantity--;
+        return true;
+    }
+
+    /**
+     * Método que permite obtener los datos de todos los usuarios en la lista.
+     * @return Retorna un arreglo de String con los datos de los usuarios.
+     */
+    public String[] getUsersAlphabetically(){
+        User[] users = new User[this.quantity];
+
+        // Copia los usuarios originales al nuevo arreglo.
+        for (int i = 0; i < this.quantity; i++) {
+            users[i] = this.users[i];
+        }
+
+        /*
+            Se puede hacer de esta forma también:
+            System.arraycopy(this.users, 0, users, 0, this.quantity);
+         */
+
+        // Ordena los usuarios en orden alfabético según su nombre.
+        for (int i = 0; i < this.quantity - 1; i++) {
+            // Segundo ciclo para comparar los nombres de los usuarios.
+            for (int j = 0; j < this.quantity - i - 1; j++) {
+                if (users[j].getFirstName().compareTo(users[j + 1].getFirstName()) > 0) {
+                    // Intercambia los usuarios
+                    User temp = users[j];
+                    users[j] = users[j + 1];
+                    users[j + 1] = temp;
+                }
+            }
+        }
+
+        // Crea un arreglo de String con los datos de los usuarios ordenados alfabéticamente.
+        String[] userNames = new String[this.quantity];
+        for (int i = 0; i < this.quantity; i++) {
+            userNames[i] = users[i].toString();
+        }
+
+        return userNames;
     }
 
 
-    public int getMaxQuantity() {
-        return maxQuantity;
-    }
-
-    public void setMaxQuantity(int maxQuantity) {
-        this.maxQuantity = maxQuantity;
-    }
-
+    /**
+     * Método que permite obtener la cantidad de usuarios en la lista.
+     * @return Retorna la cantidad de usuarios en la lista.
+     */
     public int getQuantity() {
         return quantity;
     }
 
+    /**
+     * Método que permite establecer la cantidad de usuarios en la lista.
+     * @param quantity Cantidad de usuarios en la lista.
+     */
     public void setQuantity(int quantity) {
         this.quantity = quantity;
     }
